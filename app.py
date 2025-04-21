8261bb3f832c1757aadfef71467ee15c2558d1f7
rmarso888883308 <cendrier.magma1t@icloud.com>
2025-04-21 02:16:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 PUSHOVER_TOKEN = "acbc3qetkd341664db3xgh3bts4ysz"
 PUSHOVER_USER = "uhe71aw8vzffvg38y8xk7vsnnafeq4"
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1361851340933959902/8nESdINoJzK72m7I9c8Kp8mr80IfuHiK3t1rICyjP3S8NQ1DPkEegjhXwhz4l0_wDPny"
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1361851344318632248/XQKadc8ykDsqKGjUVddnyu0Cds_4WIO0vc-PSm_9YS7Wv709exsD3Rk2o7PQfQO1iUHo"
 
 @app.route('/', methods=['POST'])
 def handle_webhook():
